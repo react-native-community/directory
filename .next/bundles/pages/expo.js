8586a59c7776e5eed84a7b8fe6ff5f531711b295
Brent Vatne <brentvatne@gmail.com>
2017-07-05 14:08:50 -0700

    Remove .next directory from git

--- .next/bundles/pages/expo.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/expo', function() {
-            var comp = module.exports=webpackJsonp([2],[],[248]);
-            return { page: comp.default }
-          })
-        
