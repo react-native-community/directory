c7046df3e36d74c8358c0816131de29343b4db14
Brent Vatne <brentvatne@gmail.com>
2017-07-05 16:29:29 -0700

    Scrape images from README

--- scripts/build-and-score-data.js
+++ scripts/build-and-score-data.js
@@ -4,6 +4,7 @@ import fs from 'fs';
 import jsonfile from 'jsonfile';
 import fetchGithubData from './fetch-github-data';
 import calculateScore from './calculate-score';
+import fetchReadmeImages from './fetch-readme-images';
 
 import githubRepos from '../github-repos.json';
 import debugGithubRepos from '../debug-github-repos.json';
@@ -22,21 +23,65 @@ const JSON_OPTIONS = {
 };
 
 const buildAndScoreData = async () => {
-  let data = USE_DEBUG_REPOS ? debugGithubRepos : githubRepos;
-  let expo, incompatible;
+  // Get basic data from Github API
+  console.log('** Loading data from Github');
+  let { expo, incompatible } = await loadRepositoryDataAsync();
 
+  // Scrape images from Github
+  console.log('');
+  console.log('** Scraping images from README');
+  expo = await Promise.all(
+    expo.map(repo => fetchReadmeImages(repo, repo.urls.repo))
+  );
+
+  incompatible = await Promise.all(
+    incompatible.map(repo => fetchReadmeImages(repo, repo.urls.repo))
+  );
+
+  // Calculate score
+  console.log('');
+  console.log('** Calculating scores');
+  expo = expo.map(calculateScore);
+  incompatible = incompatible.map(calculateScore);
+
+  const jsonData = {
+    expo,
+    incompatible,
+  };
+
+  return jsonfile.writeFile(
+    path.resolve('build', 'data.json'),
+    jsonData,
+    JSON_OPTIONS,
+    err => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('');
+        console.log('** Done!');
+      }
+    }
+  );
+};
+
+async function loadRepositoryDataAsync() {
+  let data = USE_DEBUG_REPOS ? debugGithubRepos : githubRepos;
   let githubResultsFileExists = false;
   try {
     fs.statSync(GITHUB_RESULTS_PATH);
     githubResultsFileExists = true;
   } catch (e) {}
 
+  let result;
   if (LOAD_GITHUB_RESULTS_FROM_DISK && githubResultsFileExists) {
-    ({ expo, incompatible } = jsonfile.readFileSync(GITHUB_RESULTS_PATH));
+    result = jsonfile.readFileSync(GITHUB_RESULTS_PATH);
     console.log('Loaded Github results from disk, skipped API calls');
   } else {
-    expo = await Promise.all(data.expo.map(fetchGithubData));
-    incompatible = await Promise.all(data.incompatible.map(fetchGithubData));
+    let expo = await Promise.all(data.expo.map(fetchGithubData));
+    let incompatible = await Promise.all(
+      data.incompatible.map(fetchGithubData)
+    );
+    result = { expo, incompatible };
 
     if (LOAD_GITHUB_RESULTS_FROM_DISK) {
       await new Promise((resolve, reject) => {
@@ -57,26 +102,7 @@ const buildAndScoreData = async () => {
     }
   }
 
-  expo = expo.map(calculateScore);
-  incompatible = incompatible.map(calculateScore);
-
-  const jsonData = {
-    expo,
-    incompatible,
-  };
-
-  return jsonfile.writeFile(
-    path.resolve('build', 'data.json'),
-    jsonData,
-    JSON_OPTIONS,
-    err => {
-      if (err) {
-        console.log(err);
-      }
-    }
-  );
-
-  return console.log('done');
-};
+  return result;
+}
 
 buildAndScoreData();
---
+++ scripts/fetch-readme-images.js
@@ -0,0 +1,50 @@
+import 'isomorphic-fetch';
+import cheerio from 'cheerio';
+
+const isLikelyUsefulImage = (image, githubUrl) => {
+  let parentHref = image.parent().attr('href');
+  let imageSrc = image.attr('src');
+  let isInHeader = image.parents('h1').length > 0;
+  let isFromRepo = imageSrc.includes(githubUrl);
+
+  if (
+    (isFromRepo && !isInHeader) ||
+    (parentHref && imageSrc && parentHref === imageSrc && !isInHeader)
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const scrapeImagesAsync = async githubUrl => {
+  let response = await fetch(githubUrl);
+  let html = await response.text();
+  let $ = cheerio.load(html);
+  let images = $('#readme').find('img');
+
+  let usefulImages = [];
+  for (let i = 0; i <= images.length - 1; i++) {
+    let image = images[i];
+    if (isLikelyUsefulImage($(image), githubUrl)) {
+      usefulImages.push(image);
+    }
+  }
+
+  let result = usefulImages.map(image => {
+    return $(image).attr('src');
+  });
+
+  return result;
+};
+
+const fetchReadmeImages = async (data, githubUrl) => {
+  let images = await scrapeImagesAsync(githubUrl);
+
+  return {
+    ...data,
+    images,
+  };
+};
+
+export default fetchReadmeImages;
