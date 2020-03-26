e75f85bd70ef2bb88d03b202d44c6722bcdf9a77
jonsamp <sampjon@gmail.com>
2020-03-25 20:46:26 -0400

    Merge empty results into Results

--- components/Results.tsx
+++ components/Results.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import { H3, A } from '../common/styleguide';
 import { Library } from '../types';
-import LibraryListEmptyState from './LibraryListEmptyState';
 import LibraryListItem from './LibraryListItem';
 
 type Props = {
@@ -11,7 +12,18 @@ export default function Results(props: Props) {
   const { libraries } = props;
 
   if (!libraries || !libraries.length) {
-    return <LibraryListEmptyState />;
+    return (
+      <View style={styles.container}>
+        <Image style={styles.img} source={require('../assets/notfound.png')} />
+        <H3 style={styles.text}>Nothing was found! Try another search.</H3>
+        <View style={{ marginTop: 20 }} />
+        <Text style={styles.text}>
+          Want to contribute a library you like? Submit a PR to the{' '}
+          <A href="https://github.com/react-native-community/react-native-directory">Github Repo</A>
+          .
+        </Text>
+      </View>
+    );
   }
 
   return (
@@ -26,3 +38,22 @@ export default function Results(props: Props) {
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    width: '100%',
+    paddingHorizontal: 24,
+    marginTop: 40,
+    marginBottom: 80,
+  },
+  img: {
+    marginTop: 48,
+    marginBottom: 24,
+    width: 64,
+    height: 64,
+  },
+  text: {
+    textAlign: 'center',
+  },
+});
