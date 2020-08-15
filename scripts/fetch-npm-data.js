c7460a9cf3a736346634d161063d2d93d43ac1b7
Bartosz Kaszubowski <gosimek@gmail.com>
2020-08-15 02:44:18 +0200

    store extracted npmPkg in lowercased form

--- scripts/fetch-npm-data.js
+++ scripts/fetch-npm-data.js
@@ -3,13 +3,13 @@ import fetch from 'isomorphic-fetch';
 import { sleep } from './build-and-score-data';
 
 const urlForPackage = npmPkg => {
-  return `https://api.npmjs.org/downloads/point/last-month/${npmPkg.toLowerCase()}`;
+  return `https://api.npmjs.org/downloads/point/last-month/${npmPkg}`;
 };
 
 const fetchNpmData = async (data, npmPkg, githubUrl) => {
   if (!npmPkg) {
     let parts = githubUrl.split('/');
-    npmPkg = parts[parts.length - 1];
+    npmPkg = parts[parts.length - 1].toLowerCase();
     data.npmPkg = npmPkg;
   }
 
