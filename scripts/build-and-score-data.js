cd2aa225ee94f5589d88a2508611dd9b303626ff
Bartosz Kaszubowski <gosimek@gmail.com>
2021-02-20 15:08:45 +0100

    revert more changes relate to the popularity

--- scripts/build-and-score-data.js
+++ scripts/build-and-score-data.js
@@ -6,7 +6,7 @@ import path from 'path';
 import debugGithubRepos from '../debug-github-repos.json';
 import githubRepos from '../react-native-libraries.json';
 import * as Strings from '../util/strings';
-import { calculateScore, calculatePopularity } from './calculate-score';
+import { calculateScore } from './calculate-score';
 import { fetchGithubData, fetchGithubRateLimit, loadGitHubLicenses } from './fetch-github-data';
 import fetchNpmData from './fetch-npm-data';
 import fetchReadmeImages from './fetch-readme-images';
@@ -55,17 +55,7 @@ const buildAndScoreData = async () => {
     try {
       return calculateScore(project);
     } catch (e) {
-      console.log(`Failed to calculate score for ${project.github.name}`, e.message);
-      console.log(project.githubUrl);
-    }
-  });
-
-  console.log('\n** Calculating popularity');
-  data = data.map(project => {
-    try {
-      return calculatePopularity(project);
-    } catch (e) {
-      console.log(`Failed to calculate popularity for ${project.github.name}`, e.message);
+      console.log(`Failed to calculate score for ${project.github.name}`);
       console.log(project.githubUrl);
     }
   });
