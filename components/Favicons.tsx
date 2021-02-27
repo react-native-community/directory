830900e7a0814dc9897ba8cc95d4632ad9f5e580
Bartosz Kaszubowski <gosimek@gmail.com>
2021-02-27 14:42:05 +0100

    fix favicon issue reported to Sentry (#571)

--- components/Favicons.tsx
+++ components/Favicons.tsx
@@ -2,8 +2,8 @@ import * as React from 'react';
 
 const Favicons = () => (
   <>
-    <link rel="icon" type="image/png" sizes="32x32" href={require('../assets/favicon-32x32.png')} />
-    <link rel="icon" type="image/png" sizes="16x16" href={require('../assets/favicon-16x16.png')} />
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
   </>
 );
 
