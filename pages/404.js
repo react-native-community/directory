48897898297c0183fa8419f36aa45f632b0349b6
Bartosz Kaszubowski <gosimek@gmail.com>
2021-04-20 12:18:22 +0200

    simplify 404 reporting

--- pages/404.js
+++ pages/404.js
@@ -1,12 +1,9 @@
 import * as Sentry from '@sentry/react';
-import { useRouter } from 'next/router';
 import React from 'react';
 
 import ErrorState from '../components/ErrorState';
 
 export default function NotFound() {
-  const router = useRouter();
-  Sentry.captureException('CE404: ' + router.pathname);
-  Sentry.captureMessage('CM404: ' + router.pathname);
+  Sentry.captureMessage('404');
   return <ErrorState statusCode={404} />;
 }
