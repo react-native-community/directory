1672d2528f337d9b8b98fa429a7169c5ee045cee
Bartosz Kaszubowski <gosimek@gmail.com>
2021-06-15 20:56:18 +0200

    bump Next to v10, update Sentry integration (#664)
    * bump Next to v10, update Sentry integration
    
    * attempt to fix Babel issue
    
    * enforce `loose` for all listed plugins

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,10 @@
-// @generated: @expo/next-adapter@2.0.4
-// Learn more: https://github.com/expo/expo/tree/master/docs/pages/versions/unversioned/guides/using-nextjs.md#shared-steps
+// Learn more: https://docs.expo.io/guides/using-nextjs
 
-module.exports = { presets: ['@expo/next-adapter/babel'] };
+module.exports = {
+  presets: ['@expo/next-adapter/babel'],
+  plugins: [
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+    ['@babel/plugin-proposal-private-methods', { loose: true }],
+    ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
+  ],
+};
