094ebed6028a4267a7428e7aadb8ed35d9f1c706
Bartosz Kaszubowski <gosimek@gmail.com>
2021-06-15 20:44:56 +0200

    enforce `loose` for all listed plugins

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,9 @@
 
 module.exports = {
   presets: ['@expo/next-adapter/babel'],
-  plugins: [['@babel/plugin-proposal-private-methods', { loose: false }]],
+  plugins: [
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+    ['@babel/plugin-proposal-private-methods', { loose: true }],
+    ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
+  ],
 };
