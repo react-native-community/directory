f60ed4471ea2e191e8eb98ba87118678d38b316d
Bartosz Kaszubowski <gosimek@gmail.com>
2021-07-19 18:29:01 +0200

    attempt to update to Next 11 and Webpack 5

--- next.config.js
+++ next.config.js
@@ -1,23 +1,26 @@
 const { withExpo } = require('@expo/next-adapter');
 const withImages = require('next-images');
 const withFonts = require('next-fonts');
+const withTM = require('next-transpile-modules')(['react-native-web']);
 
-module.exports = withExpo(
-  withImages(
-    withFonts({
-      projectRoot: __dirname,
-      productionBrowserSourceMaps: true,
-      async headers() {
-        return [
-          {
-            source: '/api/libraries',
-            headers: [
-              { key: 'Access-Control-Allow-Origin', value: '*' },
-              { key: 'Access-Control-Allow-Methods', value: 'GET,HEAD' },
-            ],
-          },
-        ];
-      },
-    })
+module.exports = withTM(
+  withExpo(
+    withImages(
+      withFonts({
+        projectRoot: __dirname,
+        productionBrowserSourceMaps: true,
+        async headers() {
+          return [
+            {
+              source: '/api/libraries',
+              headers: [
+                { key: 'Access-Control-Allow-Origin', value: '*' },
+                { key: 'Access-Control-Allow-Methods', value: 'GET,HEAD' },
+              ],
+            },
+          ];
+        },
+      })
+    )
   )
 );
