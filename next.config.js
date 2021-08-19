f21f0c0aac336457a78c91918ff0013274002fc9
Bartosz Kaszubowski <gosimek@gmail.com>
2021-08-19 19:26:29 +0200

    use next-compose-plugins, bump dependencies

--- next.config.js
+++ next.config.js
@@ -1,29 +1,26 @@
 const { withExpo } = require('@expo/next-adapter');
 const withImages = require('next-images');
 const withFonts = require('next-fonts');
+const withPlugins = require('next-compose-plugins');
 const withTM = require('next-transpile-modules')(['react-native-web']);
 
-module.exports = withTM(
-  withExpo(
-    withImages(
-      withFonts({
-        projectRoot: __dirname,
-        productionBrowserSourceMaps: true,
-        eslint: {
-          ignoreDuringBuilds: true,
+module.exports = withPlugins(
+  [withTM, [withExpo, { projectRoot: __dirname }], withImages, withFonts],
+  {
+    productionBrowserSourceMaps: true,
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+    async headers() {
+      return [
+        {
+          source: '/api/libraries',
+          headers: [
+            { key: 'Access-Control-Allow-Origin', value: '*' },
+            { key: 'Access-Control-Allow-Methods', value: 'GET,HEAD' },
+          ],
         },
-        async headers() {
-          return [
-            {
-              source: '/api/libraries',
-              headers: [
-                { key: 'Access-Control-Allow-Origin', value: '*' },
-                { key: 'Access-Control-Allow-Methods', value: 'GET,HEAD' },
-              ],
-            },
-          ];
-        },
-      })
-    )
-  )
+      ];
+    },
+  }
 );
