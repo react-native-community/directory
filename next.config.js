117ba6daebf81e64f34a3d7f34b8b807cfe5d39c
Simek <gosimek@gmail.com>
2023-05-20 19:34:53 +0200

    update Next, utilize build-in transpile, restore Babel for scripts

--- next.config.js
+++ next.config.js
@@ -2,38 +2,37 @@ const { withExpo } = require('@expo/next-adapter');
 const withPlugins = require('next-compose-plugins');
 const withFonts = require('next-fonts');
 const withImages = require('next-images');
-const withTM = require('next-transpile-modules')([
-  '@expo/html-elements',
-  '@react-native-picker/picker',
-  'react-native-safe-area-context',
-  'react-native-svg',
-  'react-native-web',
-]);
 
-module.exports = withPlugins(
-  [withTM, [withExpo, { projectRoot: __dirname }], withImages, withFonts],
-  {
-    productionBrowserSourceMaps: true,
-    swcMinify: true,
-    eslint: {
-      ignoreDuringBuilds: true,
-    },
-    images: {
-      disableStaticImages: true,
-    },
-    experimental: {
-      forceSwcTransforms: true,
-    },
-    async headers() {
-      return [
-        {
-          source: '/api/libraries',
-          headers: [
-            { key: 'Access-Control-Allow-Origin', value: '*' },
-            { key: 'Access-Control-Allow-Methods', value: 'GET,HEAD' },
-          ],
-        },
-      ];
-    },
-  }
-);
+module.exports = withPlugins([[withExpo, { projectRoot: __dirname }], withImages, withFonts], {
+  productionBrowserSourceMaps: true,
+  swcMinify: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    disableStaticImages: true,
+  },
+  transpilePackages: [
+    '@expo/html-elements',
+    '@react-native-picker/picker',
+    'react-native-safe-area-context',
+    'react-native-svg',
+    'react-native-web',
+    'react-native-web-hooks',
+    'react-native',
+  ],
+  experimental: {
+    forceSwcTransforms: true,
+  },
+  async headers() {
+    return [
+      {
+        source: '/api/libraries',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,HEAD' },
+        ],
+      },
+    ];
+  },
+});
