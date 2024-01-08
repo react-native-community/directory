9ff2510915ba0c283e053890ec64de3e89f21967
Bartosz Kaszubowski <gosimek@gmail.com>
2024-01-08 10:22:43 +0100

    switch from UA to GA4 project (#1103)

--- components/GoogleAnalytics.tsx
+++ components/GoogleAnalytics.tsx
@@ -1,16 +1,17 @@
 const getAnalyticsScript = (id: string) => {
   return `
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-ga('create', '${id}', {cookieDomain: 'auto', siteSpeedSampleRate: 100});
-ga('send', 'pageview');
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', '${id}');
 `.replace(/\n/g, '');
 };
 
 const GoogleAnalytics = ({ id }) => (
-  <script dangerouslySetInnerHTML={{ __html: getAnalyticsScript(id) }} />
+  <>
+    <script async src={`https://www.googletagmanager.com/gtag/js?id=${id}`} />
+    <script dangerouslySetInnerHTML={{ __html: getAnalyticsScript(id) }} />
+  </>
 );
 
 export default GoogleAnalytics;
