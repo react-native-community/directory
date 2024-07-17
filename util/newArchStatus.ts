042e6487ff1f69710d0d311c0e11cf072e54343e
Brent Vatne <brentvatne@gmail.com>
2024-07-17 15:03:51 -0700

    Update library status check and a number of libraries

--- util/newArchStatus.ts
+++ util/newArchStatus.ts
@@ -6,18 +6,26 @@ export enum NewArchSupportStatus {
   Untested = 'untested',
 }
 
-export function getNewArchSupportStatus({ newArchitecture, github }: Library) {
+export function getNewArchSupportStatus({ newArchitecture, github, expoGo }: Library) {
   const hasNote = typeof newArchitecture === 'string';
   if (hasNote) {
     return NewArchSupportStatus.Supported;
   }
 
-  const flag =
-    newArchitecture !== undefined
-      ? newArchitecture
-      : github.newArchitecture === true
-        ? true
-        : undefined;
+  // Assume untested unless indicated otherwise through one of the following tests
+  let flag = undefined;
+
+  if (newArchitecture !== undefined) {
+    flag = newArchitecture;
+  } else if (github.newArchitecture === true) {
+    flag = true;
+  } else if (expoGo === true) {
+    // If the library is an Expo Go library, we consider it as supporting new architecture because
+    // it is either part of the Expo SDK (and therefore supports it) or it is a JS library that
+    // does not depend on custom native code. In any cases where this doesn't prove to be true,
+    // we can override it with the newArchitecture field.
+    flag = true;
+  }
 
   switch (flag) {
     case true:
