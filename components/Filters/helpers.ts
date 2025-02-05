b8e7fa45b6ee223761feb7ea0c3171d00a59daf5
Bartosz Kaszubowski <gosimek@gmail.com>
2025-02-05 21:13:02 +0100

    differentiate supported statuses display from platforms (#1504)

--- components/Filters/helpers.ts
+++ components/Filters/helpers.ts
@@ -62,6 +62,14 @@ export const FILTER_STATUS = [
     title: 'Works with Expo Go',
   },
   {
+    param: 'hasTypes',
+    title: 'Has TypeScript types',
+  },
+  {
+    param: 'expoGo',
+    title: 'Works with Expo Go',
+  },
+  {
     param: 'wasRecentlyUpdated',
     title: 'Recently updated',
   },
