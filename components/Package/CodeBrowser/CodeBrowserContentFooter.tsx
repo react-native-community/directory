b662037001800849a88534eb923b2f35a0a7b32d
Bartosz Kaszubowski <gosimek@gmail.com>
2026-04-22 23:25:40 +0200

    add loader and change icon for images in code browser

--- components/Package/CodeBrowser/CodeBrowserContentFooter.tsx
+++ components/Package/CodeBrowser/CodeBrowserContentFooter.tsx
@@ -2,6 +2,7 @@ import { type PropsWithChildren, type ReactNode } from 'react';
 import { View } from 'react-native';
 import { type Style } from 'twrnc';
 
+import { useLayout } from '~/common/styleguide';
 import tw from '~/util/tailwind';
 
 type Props = PropsWithChildren<{
@@ -15,10 +16,12 @@ export default function CodeBrowserContentFooter({
   leftSlot = <View />,
   rightSlot = <View />,
 }: Props) {
+  const { isSmallScreen } = useLayout();
   return (
     <View
       style={[
         tw`relative flex min-h-[26px] flex-row items-center justify-between gap-3 border-t border-palette-gray2 bg-default px-3 pb-px dark:border-default`,
+        isSmallScreen && tw`border-b border-t-0`,
         style,
       ]}>
       {leftSlot}
