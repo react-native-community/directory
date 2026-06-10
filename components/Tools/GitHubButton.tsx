114c92684a35841a3fced88f576f2228c8b56131
Bartosz Kaszubowski <gosimek@gmail.com>
2026-06-10 15:13:40 +0200

    UX/A11y tweaks, Next bump (#2510)

--- components/Tools/GitHubButton.tsx
+++ components/Tools/GitHubButton.tsx
@@ -4,13 +4,15 @@ import { GitHubIcon } from '~/components/Icons';
 import tw from '~/util/tailwind';
 
 type Props = {
+  name: string;
   href: string;
 };
 
-export default function GitHubButton({ href }: Props) {
+export default function GitHubButton({ href, name }: Props) {
   return (
     <Button
       openInNewTab
+      aria-label={`${name} GitHub`}
       href={href}
       style={tw`min-h-8 flex-row gap-1.5 bg-primary-dark px-3 text-sm`}>
       <GitHubIcon style={tw`text-black`} />
