5ecfa2b830a89ffb6bdb3ac4cc58e1de92585130
Bartosz Kaszubowski <gosimek@gmail.com>
2026-06-15 13:47:28 +0200

    avoid node:path in constants

--- util/Constants.ts
+++ util/Constants.ts
@@ -1,12 +1,10 @@
-import path from 'node:path';
-
 import { type Query, type QueryOrder } from '~/types';
 import { TimeRange } from '~/util/datetime';
 
 export const NUM_PER_PAGE = 30;
 
-export const DATA_PATH = path.resolve('assets', 'data.json');
-export const CHECK_DATA_PATH = path.resolve('assets', 'check-data.json');
+export const DATA_PATH = 'assets/data.json';
+export const CHECK_DATA_PATH = 'assets/check-data.json';
 
 export const EMPTY_PACKAGE_DATA = {
   props: {},
