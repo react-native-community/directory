fa27b9c69d69e81d707996eccd08e4cf5e2d9c36
Bartosz Kaszubowski <gosimek@gmail.com>
2026-07-08 10:46:03 +0200

    add visual indication for hovered bars in charts, other tweaks (#2624)

--- components/Package/Charts/ChartTooltip.tsx
+++ components/Package/Charts/ChartTooltip.tsx
@@ -4,11 +4,16 @@ import { View } from 'react-native';
 import tw from '~/util/tailwind';
 
 export default function ChartTooltip({ children }: PropsWithChildren) {
+  const isDark = tw.prefixMatch('dark');
   return (
     <View
       style={[
-        tw`font-sans flex flex-col gap-px rounded bg-black px-2.5 py-1.5 text-xs font-light text-white dark:border dark:border-default`,
-        { boxShadow: '0 8px 24px #00000088' },
+        tw`font-sans flex flex-col gap-px rounded px-2.5 py-1.5 text-xs font-light text-white dark:border dark:border-default`,
+        {
+          boxShadow: '0 8px 24px #00000088',
+          backgroundColor: isDark ? '#000b' : '#000d',
+          backdropFilter: 'blur(10px)',
+        },
       ]}>
       {children}
     </View>
