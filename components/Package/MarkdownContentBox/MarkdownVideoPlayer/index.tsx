ae3b6a3b9b7e8cc29ccc2896f9cda55255afc3f4
Bartosz Kaszubowski <gosimek@gmail.com>
2026-07-20 19:06:58 +0200

    visual tweaks for HTML rendering in MD files, handle Video loading error

--- components/Package/MarkdownContentBox/MarkdownVideoPlayer/index.tsx
+++ components/Package/MarkdownContentBox/MarkdownVideoPlayer/index.tsx
@@ -4,8 +4,11 @@ import '@videojs/react/video/minimal-skin.css';
 
 import { Container } from '@videojs/react';
 import { Video } from '@videojs/react/video';
-import { type VideoHTMLAttributes } from 'react';
+import { useState, type VideoHTMLAttributes } from 'react';
 import { type Style } from 'twrnc';
+
+import { P } from '~/common/styleguide';
+import tw from '~/util/tailwind';
 
 import { InlinePlayer } from './InlinePlayer';
 import { MarkdownVideoPlayerUI } from './MarkdownVideoPlayerUI';
@@ -16,13 +19,28 @@ type Props = {
 };
 
 export default function MarkdownVideoPlayer({ src, style }: Props) {
+  const [hasError, setHasError] = useState(false);
   return (
     <InlinePlayer.Provider>
       <Container
         className="media-minimal-skin media-minimal-skin--video relative mt-3 max-h-[592px] max-w-full"
         style={style}>
-        <Video src={src} playsInline muted className="max-h-[592px]" />
-        <MarkdownVideoPlayerUI />
+        {hasError ? (
+          <P style={tw`text-center text-sm font-light leading-[150px] text-secondary`}>
+            Unable to load video source
+          </P>
+        ) : (
+          <>
+            <Video
+              src={src}
+              playsInline
+              muted
+              className="max-h-[592px]"
+              onError={() => setHasError(true)}
+            />
+            <MarkdownVideoPlayerUI />
+          </>
+        )}
       </Container>
     </InlinePlayer.Provider>
   );
