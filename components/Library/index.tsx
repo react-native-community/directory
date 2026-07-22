fb37d4824d449aeaa77de2f57d1faace1cb587b4
Bartosz Kaszubowski <gosimek@gmail.com>
2026-07-22 16:18:46 +0200

    add unique `alt` description for package images thumbnails

--- components/Library/index.tsx
+++ components/Library/index.tsx
@@ -125,8 +125,8 @@ function Library({
         </View>
         {!skipMetadata && Platform.OS === 'web' && library.images && library.images.length > 0 && (
           <View style={tw`mt-2 flex-row flex-wrap items-center gap-x-0.5`}>
-            {library.images.map(image => (
-              <Thumbnail key={image} url={image} />
+            {library.images.map((image, i) => (
+              <Thumbnail key={image} url={image} alt={`${library.npmPkg} screenshot ${i + 1}`} />
             ))}
           </View>
         )}
